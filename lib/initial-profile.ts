8b5e01fc5dbc33e01f71020731155e73d990e8d3
Samuel Souza <samuel_f2@icloud.com>
2024-05-03 17:05:53 -0300

    initial commit

---
+++ lib/initial-profile.ts
@@ -0,0 +1,29 @@
+import { currentUser, auth } from "@clerk/nextjs/server";
+import { db } from "@/lib/db";
+
+export const initialProfile = async () => {
+    const user = await currentUser()
+    if (!user) {
+        return auth().redirectToSignIn()
+    }
+
+
+    let profile = await db.profile.findUnique({
+        where: {
+            userId: user.id
+        }
+    })
+
+    if (!profile) {
+        profile = await db.profile.create({
+            data: {
+                userId: user.id,
+                name: `${user.firstName} ${user.lastName}`,
+                imageUrl: user.imageUrl,
+                email: user.emailAddresses[0].emailAddress
+            }
+        });
+    }
+
+    return profile;
+}
